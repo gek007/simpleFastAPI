3051018e8d72d133e31e5886682d9c7b810f702e
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-09-06 01:19:05 +0300

    add Dockerfile

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(title="Simple FastAPI Server", version="1.0.0")
 
-@app.get("/kostya")
+@app.get("/")
 async def kostya_route():
     logger.info("kostya_route endpoint was called")
     response = {"message": "Hello Kostya! Welcome to FastAPI!", "status": "success"}
     logger.info(f"kostya_route response: {response}")
     return response
+
+@app.get("/health")
+async def kostya_route_health():
+    logger.info("kostya_route endpoint was called")
+    response = {"message": "Server is live", "status": "success"}
+    logger.info(f"kostya_route response: {response}")
+    return response
+
 
 if __name__ == "__main__":
     import uvicorn
